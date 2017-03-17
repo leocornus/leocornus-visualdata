b38fe27dccfef91c4272106236d4be4d434b0954
Sean Chen <sean.chen@leocorn.com>
2017-03-17 14:23:37 -0400

    get started the new toy, bilevel sunburst

---
+++ src/bilevel-sunburst.js
@@ -0,0 +1,92 @@
+/**
+ * create the jQuery plugin bilevelSunburst, it will be very simple
+ * to use:
+ *
+ *  jQuery('circle-div-id').bilevelSunburst(options, jsonData);
+ */
+
+;(function($) {
+
+    // set the plugin name bilevelSunburst.
+    var pluginName = 'bilevelSunburst';
+    // set the default options.
+    var defaultOptions = {
+        "diameter" : 500,
+        "margin" : 20
+    };
+
+    /**
+     * the constructor for bilevelSunburst plugin.
+     */
+    function Plugin(element, options, jsonData) {
+        // same the DOM element.
+        this.element = element;
+        this.jsonData = jsonData;
+        // merge the options.
+        // the jQuery extend function, the later object will
+        // overwrite the former object.
+        this.options = $.extend({}, defaultOptions, options);
+        // set the plugin name.
+        this._name = pluginName;
+        // call the initialize function.
+        this.init();
+    }
+
+    // add the plugin to the jQuery chain.
+    $.fn[pluginName] = function(options, jsonData) {
+
+        // return to maintain the chain.
+        return this.each(function() {
+            // check the local storage index for the current
+            // element.
+            var dataKey = "plugin_" + pluginName;
+            if(!$.data(this, dataKey)) {
+                // no plugin created yet, let create a new one.
+                $.data(this, dataKey, 
+                       new Plugin(this, options, jsonData));
+            } else{
+                // replace with new one.
+                //$.data(this, "plugin_" + pluginName, 
+                //       new Plugin(this, options, jsonData));
+                // try reload for the existing plugin.
+                var plugin = $.data(this, dataKey);
+                plugin.reload(options, jsonData);
+            }
+        });
+    };
+
+    /**
+     * extend the prototype for the class method.
+     */
+    $.extend(Plugin.prototype, {
+
+        /**
+         * initialize function
+         */
+        init: function() {
+
+            var self = this;
+            var $element = $(this.element);
+
+            // save this id attribute.
+            self.attrId = $element.attr('id');
+        },
+
+        /**
+         * reload the plugin.
+         */
+        reload: function(options, jsonData) {
+
+            //console.log(jsonData);
+            var self = this;
+            // remove the existing one.
+            $('#' + self.attrId).empty();
+
+            // need merge the options with default options.
+            self.options = $.extend({}, defaultOptions, options);
+            self.jsonData = jsonData;
+            self.init();
+        }
+    });
+
+})(jQuery);
