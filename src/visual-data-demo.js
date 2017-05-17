f123f4e011283263a2f38962b4b18634fd5482b7
Sean Chen <sean.chen@leocorn.com>
2017-05-17 14:05:43 -0400

    add the date range for start date to end date, inclusive.

--- src/visual-data-demo.js
+++ src/visual-data-demo.js
@@ -6,12 +6,17 @@ jQuery(document).ready(function($) {
     //    $('#visualdata').visualData({date: "2017-03-12"}, data); 
     //});
 
+    // get dates.
+    var dates = getDates('05/07/2017', '05/09/2017');
     // let's try the d3 queue
     var queue = d3.queue();
-    queue.defer(d3.json, "sunburst/data/day-2017-05-07.json");
-    queue.defer(d3.json, "sunburst/data/day-2017-05-08.json");
-    queue.defer(d3.json, "sunburst/data/day-2017-05-09.json");
-    queue.defer(d3.json, "sunburst/data/week-2017-03-12.json");
+    dates.forEach(function(aDate, index) {
+        queue.defer(d3.json, "sunburst/data/day-" + aDate + ".json");
+    });
+    //queue.defer(d3.json, "sunburst/data/day-2017-05-07.json");
+    //queue.defer(d3.json, "sunburst/data/day-2017-05-08.json");
+    //queue.defer(d3.json, "sunburst/data/day-2017-05-09.json");
+    //queue.defer(d3.json, "sunburst/data/week-2017-03-12.json");
     queue.awaitAll(function(error, results) {
         if (error) {
             throw error;
@@ -49,3 +54,29 @@ jQuery(document).ready(function($) {
         $('#visualdata').visualData({date: "2017-03-12"}, theData); 
     });
 });
+
+/**
+ * define the function to get date range.
+ *   getDates('05/01/2017', '05/15/2017');
+ * inclusive!
+ */
+var getDates = function(startDate, endDate) {
+    var current = new Date(startDate);
+    var end = new Date(endDate); 
+    var dates = [];
+
+    while(current <= end) {
+
+        var m = current.getMonth() + 1;
+        var d = current.getDate();
+        var oneDate = [
+            current.getFullYear(),
+            m < 10 ? "0" + m : m,
+            d < 10 ? "0" + d : d].join('-');
+
+        dates.push(oneDate);
+        current.setDate(current.getDate() + 1);
+    }
+
+    return dates;
+};
